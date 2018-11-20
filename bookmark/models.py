da51c882cbffc562426e0b4289050d35e015b467
totoro <totoro@smartstudy.co.kr>
2018-11-20 10:44:01 +0900

    Revert "no message"
    This reverts commit f3bf727a08dad9419a924a111e91a092e74d47e7.

--- bookmark/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Bookmark(models.Model):
-    title = models.CharField(max_length=100, blank=True,null=True)
-    url= models.URLField('url', unique=True)
-
-    def __str__(self):
-        return "{} {}".format(self.title, self.url)
